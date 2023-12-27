3b4bb815905baa139649bfb3193249696e4c7f4b
nvhung1810 <nguyenvanhung181001@gmail.com>
2023-12-27 15:24:16 +0700

    feat: creating a vue application

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-// import the root component App from a single-file component.
+/**
+ * import the root component App from a single-file component.
+ * nhập ứng dụng thành phần gốc từ một thành phần một tệp.
+ */
 import App from './App.vue'
 
+const app = createApp(App)
 /**
-  mot phien ban ung dung se khong hien thi cai j cho den khi phuong thuc
-  .mount() duoc goi. No mong doi mot doi so "container", co the la
-  phan tu DOM thuc te hoac chuoi bo chon
-  --> Tom lai la khi nao co .mount() thi no moi dc goi
-
-  ##### <div id="app"></div>
-  ##### app.mount('#app')
- */
-
-
-createApp(App).mount('#app')
-
-/**
-    App (root component)
+ * App (root component)
     ├─ TodoList
     │  └─ TodoItem
     │     ├─ TodoDeleteButton
@@ -26,4 +17,26 @@ createApp(App).mount('#app')
     └─ TodoFooter
       ├─ TodoClearButton
       └─ TodoStatistics
+ */
+
+// ###: Mounting the App
+/**
+ * Một phiên bản ứng dụng sẽ không hiển thị bất kỳ thứ gì cho đến khi phương thức .mount() của nó được gọi.
+ * Nó mong đợi một đối số "container", có thể là một phần tử DOM thực tế hoặc một chuỗi bộ chọn:
+ */
+
+// <div id="app"></div>
+app.mount('#app')
+
+/**
+ * ###: Multiple application instances
+ *  const app1 = createApp({
+      .......
+    })
+    app1.mount('#container-1')
+
+    const app2 = createApp({
+      .......
+    })
+    app2.mount('#container-2')
  */
